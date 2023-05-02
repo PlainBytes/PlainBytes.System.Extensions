939cfac29a3b875fbb40d9df4718b45364a5bbe1
Árpád Barta <arpad.barta@outlook.com>
2023-05-02 21:21:19 +0300

    Removes over lapping methods

--- source/PlainBytes.System.Extensions/Collections/Iterators.cs
+++ source/PlainBytes.System.Extensions/Collections/Iterators.cs
@@ -17,21 +17,6 @@ namespace PlainBytes.System.Extensions.Collections
         /// <param name="collection">Collection on which we iterate.</param>
         /// <param name="action">The action that will be executed for each and every element.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void For<T>(this IList<T> collection, Action<int, T> action)
-        {
-            for (var i = 0; i < collection.Count; i++)
-            {
-                action(i, collection[i]);
-            }
-        }
-
-        /// <summary>
-        /// Iterates through the collection while calling the action with the current index and element.
-        /// </summary>
-        /// <typeparam name="T">The base type of the source collection elements.</typeparam>
-        /// <param name="collection">Collection on which we iterate.</param>
-        /// <param name="action">The action that will be executed for each and every element.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void For<T>(this IEnumerable<T> collection, Action<int, T> action)
         {
             var index = 0;
@@ -42,24 +27,7 @@ namespace PlainBytes.System.Extensions.Collections
                 index++;
             }
         }
-        
-        /// <summary>
-        /// Iterates through the collection while calling the function with the current index and element.
-        /// </summary>
-        /// <typeparam name="T">The base type of the source collection elements.</typeparam>
-        /// <typeparam name="TR">The base type of the collection elements that will be returned</typeparam>
-        /// <param name="collection">Collection on which we iterate.</param>
-        /// <param name="function">The function that will be executed for each and every element and its result is yielded</param>
-        /// <returns>Collection of results</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IEnumerable<TR> SelectWithIndex<T, TR>(this IList<T> collection, Func<int, T, TR> function)
-        {
-            for (var i = 0; i < collection.Count; i++)
-            {
-                yield return function(i, collection[i]);
-            }
-        }
-        
+
         /// <summary>
         /// Iterates through the collection while calling the function with the current index and element.
         /// </summary>
