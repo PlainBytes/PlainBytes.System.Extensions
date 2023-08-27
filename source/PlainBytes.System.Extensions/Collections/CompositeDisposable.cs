6f86d7c748c9cdd26ac9cd8f3fd004c7356d834a
Árpád Barta <arpad.barta@outlook.com>
2023-08-27 14:46:09 +0300

    Adds CompositeDisposable

---
+++ source/PlainBytes.System.Extensions/Collections/CompositeDisposable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PlainBytes.System.Extensions.Collections
+{
+    /// <summary>
+    /// Allows disposing multiple <see cref="IDisposable"/>s at the same time.
+    /// </summary>
+    public class CompositeDisposable : IDisposable
+    {
+        private readonly IDisposable[] _disposables;
+
+        /// <summary>
+        /// Creates an instance from the provided disposables.
+        /// </summary>
+        public CompositeDisposable(params IDisposable[] disposables)
+        {
+            _disposables = disposables ?? throw new ArgumentNullException(nameof(disposables));
+        }
+        
+        /// <summary>
+        /// <inheritdoc cref="IDisposable.Dispose"/>
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var disposable in _disposables)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
