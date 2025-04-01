3ddb84bd91920b888875b0da0912d50a8cf392a1
Árpád Barta <arpad.barta@outlook.com>
2025-04-01 15:26:58 +0300

    Adds conversion to bytes for double and int types

--- source/PlainBytes.System.Extensions/BaseTypes/IntExtensions.cs
+++ source/PlainBytes.System.Extensions/BaseTypes/IntExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace PlainBytes.System.Extensions.BaseTypes
@@ -37,5 +38,48 @@ namespace PlainBytes.System.Extensions.BaseTypes
         /// <returns><inheritdoc cref="Convert.ToBoolean(uint)"/></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool ToBool(this uint value) => Convert.ToBoolean(value);
+
+        /// <summary>
+        /// Converts the provided ints into bytes.
+        /// </summary>
+        /// <param name="value">Values which should be converted into bytes.</param>
+        /// <returns>Collection of <see langword="byte"/>s.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if the provided value is <see langword="null"/></exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte[] GetBytes(this int[] value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var numArray = new byte[value.Length * 4];
+            Buffer.BlockCopy(value, 0, numArray, 0, numArray.Length);
+            return numArray;
+        }
+
+        /// <summary>
+        /// Converts the provided bytes into ints.
+        /// </summary>
+        /// <param name="value">Values which should be converted into ints.</param>
+        /// <returns>Collection of <see langword="int"/>s.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if the provided value is <see langword="null"/>.</exception>
+        /// <exception cref="InvalidDataException">Thrown if the number of bytes does not align with the value type.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int[] ToIntArray(this byte[] value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if (value.Length % 4 != 0)
+            {
+                throw new InvalidDataException("Byte Object length must be a multiple of 4");
+            }
+
+            var result = new int[value.Length / 4];
+
+            for (var i = 0; i < value.Length; i += 4)
+            {
+                result[i / 4] = BitConverter.ToInt32(value[i..(i + 4)]);
+            }
+
+            return result;
+        }
     }
 }
---
+++ tests/PlainBytes.System.Extensions.Tests/BaseTypes/IntegerExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using PlainBytes.System.Extensions.BaseTypes;
+using Xunit;
+
+namespace PlainBytes.System.Extensions.Tests.BaseTypes
+{
+    public class IntegerExtensionsTests
+    {
+        [Fact]
+        public void Integers_ToBytes_RoundTrip()
+        {
+            // Arrange
+            var expectedResult = Enumerable.Range(0, 100).Select(_ => Random.Shared.Next()).ToArray();
+
+            // Act
+            var bytes = expectedResult.GetBytes();
+            var result = bytes.ToIntArray();
+
+            // Assert
+            Assert.True(expectedResult.SequenceEqual(result));
+        }
+    }
+}
